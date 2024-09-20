d68f347e7a470931c7a7a595fb1794a216ea385b
rayajose <162737338+rayajose@users.noreply.github.com>
2024-09-20 08:16:32 -0400

    add oxygen writing sample project

---
+++ doc/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,16 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Partner",p:["p1"]},{w:"Onboarding",p:["p1"]},{w:"Error",p:["p2"]},{w:"Responses",p:["p2"]},{w:"Authentication",p:["p3"]},{w:"Overview",p:["p4"]},{w:"REST",p:["p5","p6"]},{w:"Resource:",p:["p5","p6"]},{w:"needles",p:["p5"]},{w:"xmen",p:["p6"]},{w:"Process",p:["p7"]},{w:"Documentation",p:["p7","p8"]},{w:"Reference",p:["p8"]},{w:"Change",p:["p9"]},{w:"Management",p:["p9"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4];
+ph["p2"]=[5, 6];
+ph["p3"]=[7];
+ph["p4"]=[8];
+ph["p5"]=[9, 10, 11];
+ph["p6"]=[9, 10, 12];
+ph["p7"]=[13, 14];
+ph["p8"]=[15, 14];
+ph["p9"]=[16, 17];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ doc/out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {9:7,1:7,7:-1,8:-1,6:8,4:8,2:8,5:8,3:8};
