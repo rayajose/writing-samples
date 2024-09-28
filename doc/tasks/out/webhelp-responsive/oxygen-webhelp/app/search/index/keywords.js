a6b72f70354f7b12fbe8c2f2179165113d3bdc10
rayajose <162737338+rayajose@users.noreply.github.com>
2024-09-28 17:21:15 -0400

    updates

--- doc/tasks/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ doc/tasks/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,7 +1,15 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Create",p:["p1"]},{w:"a",p:["p1"]},{w:"Collection",p:["p1"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Creating",p:["p1"]},{w:"a",p:["p1","p2"]},{w:"Postman",p:["p1"]},{w:"Collection",p:["p1","p2"]},{w:"Create",p:["p2"]},{w:"Testing",p:["p3","p4","p5","p6","p7"]},{w:"DELETE/{id}",p:["p3"]},{w:"Request",p:["p3","p4","p5","p6","p7","p9"]},{w:"GET/{id}",p:["p4"]},{w:"GET",p:["p5"]},{w:"POST",p:["p6"]},{w:"PUT/{id}",p:["p7"]},{w:"***",p:["p8"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
-ph["p1"]=[3, 4, 5];
+ph["p1"]=[3, 4, 5, 6];
+ph["p2"]=[7, 4, 6];
+ph["p3"]=[8, 9, 10];
+ph["p4"]=[8, 11, 10];
+ph["p5"]=[8, 12, 10];
+ph["p6"]=[8, 13, 10];
+ph["p7"]=[8, 14, 10];
+ph["p8"]=[15];
+ph["p9"]=[10];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
