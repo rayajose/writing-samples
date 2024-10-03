6fa353dbb9599623675b974fa30d3a3c8b1b67c7
rayajose <162737338+rayajose@users.noreply.github.com>
2024-10-03 12:30:33 -0400

    updates

--- doc/out/webhelp-all/oxygen-webhelp/app/search/index/link-to-parent.js
+++ doc/out/webhelp-all/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {7:-1,10:-1,11:-1,8:-1,9:-1,13:-1,12:-1};
+var linkToParent = {10:-1,8:-1,13:-1,9:-1,11:-1,7:-1,12:-1};
