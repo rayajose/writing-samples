edbddf9f225a6e6f9badab95f64df7d0c3f2f9e2
rayajose <162737338+rayajose@users.noreply.github.com>
2024-10-03 11:32:33 -0400

    updates

--- doc/out/webhelp-test/oxygen-webhelp/app/search/index/keywords.js
+++ doc/out/webhelp-test/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"AES-256",p:["p1"]},{w:"Cryptanalysis",p:["p2"]},{w:"cryptoperiod",p:["p3"]},{w:"DEK",p:["p4"]},{w:"KEK",p:["p5"]},{w:"PCI",p:["p6"]},{w:"DSS",p:["p6"]},{w:"About",p:["p7"]},{w:"the",p:["p7"]},{w:"writing",p:["p7"]},{w:"samples",p:["p7"]},{w:"API",p:["p8","p11"]},{w:"testing",p:["p8"]},{w:"with",p:["p8","p10"]},{w:"Postman",p:["p8"]},{w:"Cryptographic",p:["p9"]},{w:"key",p:["p9"]},{w:"management",p:["p9"]},{w:"Shop",p:["p10"]},{w:"Points\u00AE",p:["p10"]},{w:"partner",p:["p10"]},{w:"onboarding",p:["p10"]},{w:"My",p:["p11","p12"]},{w:"REST",p:["p11"]},{w:"Java",p:["p12"]},{w:"class",p:["p12"]},{w:"library",p:["p12"]},{w:"Request",p:["p13"]},{w:"Product",p:["p14"]},{w:"catalog",p:["p14"]},{w:"feed",p:["p14"]},{w:"specification",p:["p14"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"AES-256",p:["p1"]},{w:"Cryptanalysis",p:["p2"]},{w:"cryptoperiod",p:["p3"]},{w:"DEK",p:["p4"]},{w:"KEK",p:["p5"]},{w:"PCI",p:["p6"]},{w:"DSS",p:["p6"]},{w:"Creating",p:["p7"]},{w:"a",p:["p7"]},{w:"Postman",p:["p7","p14"]},{w:"collection",p:["p7"]},{w:"Testing",p:["p8","p9","p10","p11","p12","p14"]},{w:"the",p:["p8","p9","p10","p11","p12","p13"]},{w:"DELETE/{id}",p:["p8"]},{w:"request",p:["p8","p9","p10","p11","p12"]},{w:"GET/{id}",p:["p9"]},{w:"GET",p:["p10"]},{w:"POST",p:["p11"]},{w:"PUT/{id}",p:["p12"]},{w:"About",p:["p13"]},{w:"writing",p:["p13"]},{w:"samples",p:["p13"]},{w:"API",p:["p14","p17"]},{w:"with",p:["p14","p16"]},{w:"Cryptographic",p:["p15"]},{w:"key",p:["p15"]},{w:"management",p:["p15"]},{w:"Shop",p:["p16"]},{w:"Points\u00AE",p:["p16"]},{w:"partner",p:["p16"]},{w:"onboarding",p:["p16"]},{w:"My",p:["p17","p18"]},{w:"REST",p:["p17"]},{w:"Java",p:["p18"]},{w:"class",p:["p18"]},{w:"library",p:["p18"]},{w:"Product",p:["p19"]},{w:"catalog",p:["p19"]},{w:"feed",p:["p19"]},{w:"specification",p:["p19"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3];
@@ -9,12 +9,17 @@ ph["p5"]=[7];
 ph["p6"]=[8, 9];
 ph["p7"]=[10, 11, 12, 13];
 ph["p8"]=[14, 15, 16, 17];
-ph["p9"]=[18, 19, 20];
-ph["p10"]=[21, 16, 22, 23, 24];
-ph["p12"]=[25, 27, 28, 29];
-ph["p11"]=[25, 26, 14];
-ph["p14"]=[31, 32, 33, 34];
-ph["p13"]=[30];
+ph["p9"]=[14, 15, 18, 17];
+ph["p10"]=[14, 15, 19, 17];
+ph["p12"]=[14, 15, 21, 17];
+ph["p11"]=[14, 15, 20, 17];
+ph["p14"]=[25, 14, 26, 12];
+ph["p13"]=[22, 15, 23, 24];
+ph["p16"]=[30, 26, 31, 32, 33];
+ph["p15"]=[27, 28, 29];
+ph["p18"]=[34, 36, 37, 38];
+ph["p17"]=[34, 35, 25];
+ph["p19"]=[39, 40, 41, 42];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
--- doc/out/webhelp-test/oxygen-webhelp/app/search/index/link-to-parent.js
+++ doc/out/webhelp-test/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {11:-1,14:-1,9:-1,8:-1,7:-1,10:-1,12:-1};
+var linkToParent = {8:14,17:-1,13:-1,7:14,14:-1,15:-1,10:14,12:14,16:-1,18:-1,19:-1,11:14,9:14};
